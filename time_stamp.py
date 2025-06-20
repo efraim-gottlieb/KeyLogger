66fa2e6e2835421db4c68d368146839c6dc7092e
Israel Shvadron <srulikshv@gmail.com>
2025-06-20 15:21:30 +0300

    time_stamp

---
+++ time_stamp.py
@@ -0,0 +1,14 @@
+from time import localtime as T, sleep
+
+def time_stamp():
+    s='*'*6
+    sl='/'
+    a = [s, ' ', str(T()[2]), sl, str(T()[1]), sl, str(T()[0]), ' ', str(T()[3]), ':', str(T()[4]), ':', str(T()[5]),' ', s]
+    return ''.join(a)
+if __name__ == '__main__':
+
+    while 1:
+
+         sleep(2)
+         print(time_stamp())
+
