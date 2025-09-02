c28c9f7474ee30306ef7d96e2737f112be1c96c2
efraim gottlieb <efgo583208979@gmail.com>
2025-09-02 18:20:20 +0300

    fix location style

--- data_collector.py
+++ data_collector.py
@@ -10,11 +10,13 @@ class Package:
         data_info = SuspiciousTextChecker(text=str(self.data),file_path="suspicious_words.txt")
         self.label = data_info.is_suspicious()
         self.suspicious_words = data_info.find_suspicious()
-        self.pc_info = get_system_info()["computer_name"]
+        self.pc_info = get_system_info()
 
     def export_package(self):
         return {
-            "computer name":self.pc_info,
+            "computer name":self.pc_info['computer_name'],
+            "country" : self.pc_info['location']['country'],
+            "city" : self.pc_info['location']['city'],
             "time" : self.time_stamp,
             "label" : self.label,
             "suspicious words" : self.suspicious_words,
@@ -25,4 +27,4 @@ if __name__ == '__main__':
     d = {'fr':'hack', 'moi':'hack'}
     a = Package(data=d)
     print(a.export_package()['data'])
-
+    print(a.export_package())
