9e9a8a147499f4df00af8a8a2ed8c55b6b0b5140
Israel Shvadron <srulikshv@gmail.com>
2025-09-02 11:30:39 +0300

    fix encrypt

--- encrypt.py
+++ encrypt.py
@@ -1,23 +1,41 @@
+from random import randint
 class Encrypt:
     def __init__(self,key='kodkodecycled2025'):
         self.key=key
+        if key=='RAND':
+            self.key=''
+            while len(self.key)<=100:
+                self.key+=str(randint(0,100))
+    def xor_encrypt(self, text):
+        self.tempkey = self.key * (len(text) // len(self.key)) + self.key[:(len(text) % len(self.key))]
+        self.t_encrypted = ''.join(chr(ord(x) ^ ord(y)) for x, y in zip(text, self.tempkey))
     def encrypt_text(self,text):
-        tempkey=self.key*(len(text)//len(self.key))+ self.key[:(len(text)%len(self.key))]
-        self.encrypted=[]
-        for x,y in zip(text,tempkey):
-            self.encrypted.append(chr(ord(x)^ord(y)))
-        self.encrypted=''.join(self.encrypted)
-    def decrypt_text(self,text):
-        tempkey=self.key*(len(text)//len(self.key))+ self.key[:(len(text)%len(self.key))]
-        self.decrypted=[]
-        for x,y in zip(text,tempkey):
-            self.decrypted.append(chr(ord(x)^ord(y)))
-        self.decrypted=''.join(self.decrypted)
+        self.xor_encrypt(text)
+        self.encrypted = self.t_encrypted
+
+    def decrypt_text(self, text):
+        self.xor_encrypt(text)
+        self.decrypted = self.t_encrypted
 if __name__==("__main__"):
     c=Encrypt()
-    c.encrypt_text('my name is srulik shvadron hi am 26 age')
+    c.encrypt_text('my name is srulik shvadron i am 26 age')
     b=c.encrypted
-    h=Encrypt()
     c.decrypt_text(b)
-    print(b)
-    print(c.decrypted)
+    r=Encrypt('RAND')
+    r.encrypt_text('my name is srulik shvadron i am 26 age')
+    b=r.encrypted
+    r.decrypt_text(b)
+    s = Encrypt('hii ')
+    s.encrypt_text('my name is srulik shvadron i am 26 age')
+    b = s.encrypted
+    s.decrypt_text(b)
+
+    print(f'c: {c.encrypted}\n{c.decrypted}\n'
+          f'r: {r.encrypted}\n{r.decrypted}\n'
+          f's: {s.encrypted}\n{s.decrypted}\n')
+
+
+
+
+
+
