7ffff208d91af7bb2f2f02e67500274b83d9edf3
efraim gottlieb <efgo583208979@gmail.com>
2025-09-02 18:21:32 +0300

    add time-stamp style

--- time_stamp.py
+++ time_stamp.py
@@ -7,6 +7,11 @@ def time_stamp():
     a = [s, ' ', str(T()[2]), sl, str(T()[1]), sl, str(T()[0]), ' ', str(T()[3]), ':', str(T()[4]), ':', str(T()[5]),' ', s]
     return ''.join(a)
 
+def time_stamp2():
+    s=''
+    sl='.'
+    a = [s, ' ', str(T()[2]), sl, str(T()[1]), sl, str(T()[0]), '_', str(T()[3]), '.', str(T()[4]), '.', str(T()[5]),' ', s]
+    return ''.join(a)
 
 
 
@@ -14,5 +19,5 @@ if __name__ == '__main__':
 
     while 1:
          sleep(2)
-         print(time_stamp())
+         print(time_stamp2())
 
